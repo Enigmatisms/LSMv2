1e1bdf19e121a413ce5af07b9271a5c5ed5d86d4
Enigmatisms <984041003@qq.com>
2022-10-14 16:09:36 +0800

    Separate build

--- lsm/build.rs
+++ lsm/build.rs
@@ -9,7 +9,15 @@ fn main() {
         .flag("arch=compute_86,code=sm_86")
         .file("cuda/ray_tracer.cu")
         .file("cuda/host_func.cu")
+        .compile("libcuda_helper.a");
+
+    cc::Build::new()
+        .cuda(true)
+        .flag("-lcuda")
+        .flag("-lcudart")
+        .flag("-gencode")
+        .flag("arch=compute_86,code=sm_86")
         .file("cuda/cast_kernel.cu")
         .file("cuda/shadow_cast.cu")
-        .compile("libcuda_helper.a");
+        .compile("libcast_helper.a");
 }
